f5f609816115e27482175aa226fa2d10e20fea0c
Bernard Huff <bernard.huff008@gmail.com>
2017-07-29 09:27:35 -0500

    Basic functions huh?

--- functionPractice.playground/Contents.swift
+++ functionPractice.playground/Contents.swift
@@ -2,23 +2,40 @@
 
 //basic function code
 
-
+//So you think these functions are basic? 
 
 //"triple" is function name, "(..)" are the parameters
+
 func triple(value:Int) {
+    
     let result = value*3
+    
     print("If you multiply \(value) by 3, you'll get \(result).")
+    
 }
 
 //call the function while setting the parameter at the same time
+
 triple(value:10)
 
-
-
-
 //same thing, this time we doing multiple arguments/parameters
+
+//First off...I hate your arguments and function calls. lol 
+
 func multiply (firstNumber:Int, secondNumber:Int) {
+    
     let result = firstNumber * secondNumber
+    
     print("The result is \(result).")
 }
+
 multiply(firstNumber:10, secondNumber:5)
+
+
+
+
+
+
+
+
+
